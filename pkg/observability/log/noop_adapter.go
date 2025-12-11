9af271aa77d4b24792ea47fafdbf9fb7c0e7c274
Adam Holt <me@adamholt.co.uk>
2025-12-11 15:24:40 +0100

    Nothing is used here, so we can ignore the parameters for linter issues.

--- pkg/observability/log/noop_adapter.go
+++ pkg/observability/log/noop_adapter.go
@@ -17,43 +17,43 @@ func (l *NoopLogger) Level() Level {
 	return DebugLevel
 }
 
-func (l *NoopLogger) Log(ctx context.Context, level Level, msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Log(_ context.Context, _ Level, _ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) Debug(msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Debug(_ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) Info(msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Info(_ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) Warn(msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Warn(_ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) Error(msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Error(_ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) Fatal(msg string, fields ...slog.Attr) {
+func (l *NoopLogger) Fatal(_ string, _ ...slog.Attr) {
 	// No-op
 }
 
-func (l *NoopLogger) WithFields(fields ...slog.Attr) Logger {
+func (l *NoopLogger) WithFields(_ ...slog.Attr) Logger {
 	return l
 }
 
-func (l *NoopLogger) WithError(err error) Logger {
+func (l *NoopLogger) WithError(_ error) Logger {
 	return l
 }
 
-func (l *NoopLogger) Named(name string) Logger {
+func (l *NoopLogger) Named(_ string) Logger {
 	return l
 }
 
-func (l *NoopLogger) WithLevel(level Level) Logger {
+func (l *NoopLogger) WithLevel(_ Level) Logger {
 	return l
 }
 
